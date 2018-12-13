159aeff4443496257aeef3643f1eb1e15d9cd0e8
HaHussain <HaHussain010@gmail.com>
2018-12-13 22:43:37 +0000

    Incomplete Commit, converting Lists to Dictionary

--- XML to JSON Converter.py
+++ XML to JSON Converter.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 
 MonsterList = []
@@ -47,20 +48,32 @@ def SplitAbilities(Tag, MonsterText):
     return AbilityList
 
 def DealWithMonster(MonsterText):
-    CreatureAttributes = []
-    CreatureAttributes.append(XMLRead("name", MonsterText)) #name
+    CreatureAttributes = {}
+    CreatureAttributes["Name"] = (XMLRead("name", MonsterText)) #name
     CreatureAttributes.append(XMLRead("size", MonsterText)) #size
     CreatureAttributes += XMLRead("type", MonsterText).split(', ') #creature type and book name
     CreatureAttributes.append(XMLRead("alignment", MonsterText)) #alignment
-    CreatureAttributes.append(XMLRead("ac", MonsterText)) #ac
-    CreatureAttributes.append(XMLRead("hp", MonsterText)) #hp
+    CreatureAttributes["AC"] = {}
+    CreatureAC = (XMLRead("ac", MonsterText)).split(' ') #ac
+    if len(CreatureAC) > 1:
+        CreatureAttributes["AC"]["Notes"] = CreatureAC[1][1:-1]
+    CreatureAttributes["AC"]["Value"] = CreatureAC[0]
+    CreatureAttributes["HP"] = {}
+    CreatureHP = re.split('+ | ',(XMLRead("hp", MonsterText)))
+    if len(CreatureHP) > 1:
+        if CreatureHP[1][-1] == ")":
+            CreatureAttributes["HP"]["Notes"] = CreatureHP[1][1:-1]
+        else:
+            CreatureAttributes["HP"]["Notes"] = CreatureHP[1][1:]
+    CreatureAttributes["HP"]["Value"] = CreatureHP[0]
     CreatureAttributes.append(XMLRead("speed", MonsterText).split(', '))
-    CreatureAttributes.append(XMLRead("str", MonsterText))
-    CreatureAttributes.append(XMLRead("dex", MonsterText))
-    CreatureAttributes.append(XMLRead("con", MonsterText))
-    CreatureAttributes.append(XMLRead("int", MonsterText))
-    CreatureAttributes.append(XMLRead("wis", MonsterText))
-    CreatureAttributes.append(XMLRead("cha", MonsterText))
+    CreatureAttribures["Abilities"] = {}
+    CreatureAttributes["Abilities"]["Str"] = (XMLRead("str", MonsterText))
+    CreatureAttributes["Abilities"]["Dex"] = (XMLRead("dex", MonsterText))
+    CreatureAttributes["Abilities"]["Con"] = (XMLRead("con", MonsterText))
+    CreatureAttributes["Abilities"]["Int"] = (XMLRead("int", MonsterText))
+    CreatureAttributes["Abilities"]["Wis"] = (XMLRead("wis", MonsterText))
+    CreatureAttributes["Abilities"]["Cha"] = (XMLRead("cha", MonsterText))
     CreatureAttributes.append(XMLRead("save", MonsterText).split(', '))
     CreatureAttributes.append(XMLRead("skill", MonsterText).split(', '))
     CreatureAttributes.append(MonsterElementalAffinities("resist", MonsterText))
@@ -69,7 +82,7 @@ def DealWithMonster(MonsterText):
     CreatureAttributes.append(XMLRead("conditionImmune", MonsterText).split(', '))
     CreatureAttributes.append(XMLRead("senses", MonsterText).split(', '))
     CreatureAttributes.append(XMLRead("languages", MonsterText).split(', '))
-    CreatureAttributes.append(XMLRead("cr", MonsterText))
+    CreatureAttributes["Challenge"] = (XMLRead("cr", MonsterText))
 
     CreatureAttributes.append(SplitAbilities("trait", MonsterText))
     CreatureAttributes.append(SplitAbilities("action", MonsterText))
@@ -91,18 +104,3 @@ while True:
         break
 XML.close()
 JSON = open(".\\myMonsters.json","w+")
-JSON.writelines("[\n")
-for Monster in MonsterList:
-        JSON.writelines("   {\n")
-        JSON.writelines("       \"AC\":{\n")
-        JSON.writelines("           \"Notes\":\""++"\",\n") #TODO
-        JSON.writelines("           \"Value\":\""++"\",\n") #TODO
-        JSON.writelines("       },\n")
-        JSON.writelines("       \"Abilities\":{\n")
-        JSON.writelines("           \"Str\":\"%s\",\n"%(Monster[8]))
-        JSON.writelines("           \"Dex\":\"%s\",\n"%(Monster[9]))
-        JSON.writelines("           \"Con\":\"%s\",\n"%(Monster[10]))
-        JSON.writelines("           \"Int\":\"%s\",\n"%(Monster[11]))
-        JSON.writelines("           \"Wis\":\"%s\",\n"%(Monster[12]))
-        JSON.writelines("           \"Cha\":\"%s\",\n"%(Monster[13]))
-        JSON.writelines("       },\n")
